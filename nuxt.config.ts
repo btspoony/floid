7b2e7f95209c9e5b3c36b0b432368a065a5ce7d8
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-05-17 16:38:39 +0800

    chore: fix cdc and node

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from "nuxt";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -14,4 +15,22 @@ export default defineNuxtConfig({
   },
   // Environment Variables
   runtimeConfig: {},
+  // vite configure
+  vite: {
+    // raw assets
+    assetsInclude: ["**/*.cdc"],
+    // Dependency Pre-Bundling
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+          }),
+        ],
+      },
+    },
+  },
 });
