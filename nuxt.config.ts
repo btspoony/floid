01f48db96d40c94de34ba1579b562a55cb0d43d7
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-06-09 00:26:58 +0800

    feat: add ga

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
       hostUrl: "",
       floidAddress: "",
       avatarUrl: "",
+      gaId: "",
     },
   },
   // ts config
--- src/plugins/misc.ts
+++ src/plugins/misc.ts
@@ -4,5 +4,6 @@ import { Web3ModalVuePlugin } from "web3modal-vue3";
 export default defineNuxtPlugin((nuxtApp) => {
   // image lazy load
   nuxtApp.vueApp.use(VueLazyLoad);
+  // web3modal
   nuxtApp.vueApp.use(Web3ModalVuePlugin);
 });
