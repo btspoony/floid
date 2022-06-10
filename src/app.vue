06c8db88e0c9db5bc9d3cb20317680866d60113d
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-06-11 00:18:45 +0800

    feat: separate base meta

--- src/app.vue
+++ src/app.vue
@@ -2,29 +2,20 @@
   <div>
     <!-- override html and head -->
     <Html :lang="locale">
-
-    <Head>
-      <Title>{{ route.meta.title ?? "Home" }}</Title>
-      <Meta name="description" :content="description" />
-      <Meta property="og:type" content="article" />
-      <Meta property="og:title" :content="`${route.meta.title ?? 'Home'} - Floid`" />
-      <Meta property="og:description" :content="description" />
-      <Meta property="og:url" :content="'https://' + website" />
+    <BaseMeta :title="route.meta.title ?? 'Home'" :description="description" :url="route.fullPath">
       <Meta property="og:site_name" :content="website" />
       <Meta property="og:image" :content="`http://${website}/social-card.jpg`" />
       <!-- Twitter related -->
       <Meta name="twitter:image" :content="`http://${website}/social-card.jpg`" />
       <Meta name="twitter:card" content="summary_large_image" />
       <Meta name="twitter:image:alt" :content="website" />
-      <Meta name="twitter:title" :content="description" />
-      <Meta name="twitter:description" :content="description" />
       <Meta name="twitter:site" content="@btspoony" />
       <!-- Favicons -->
       <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <Link rel="manifest" href="/site.webmanifest" />
-    </Head>
+    </BaseMeta>
 
     </Html>
 
---
+++ src/components/base/Meta.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="author" :content="author" />
+    <Meta name="description" :content="description" />
+
+    <!--  Essential META Tags -->
+    <Meta property="og:type" content="article" />
+    <Meta property="og:title" :content="`${title} - Floid`" />
+    <Meta property="og:description" :content="description" />
+    <Meta property="og:url" :content="url" />
+
+    <!-- Twitter related -->
+    <Meta name="twitter:title" :content="title" />
+    <Meta name="twitter:description" :content="description" />
+
+    <!-- Other meta -->
+    <slot />
+  </Head>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+withDefaults(
+  defineProps<{
+    title: string;
+    description: string;
+    url: string;
+    author?: string;
+  }>(),
+  {
+    author: "floid.pro",
+  }
+);
+</script>
